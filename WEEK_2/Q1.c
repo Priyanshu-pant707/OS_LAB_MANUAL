c195792b5908a09028176d99f9ecfdd68adf9a00
PRIYANSHU_PANT <pantpriyanshu707@gmail.com>
2025-07-30 17:43:14 +0000

    week 2 question  1

---
+++ WEEK_2/Q1.c
@@ -0,0 +1,32 @@
+/*
+
+Write a program to open a directory and list its contents. (use opendir(), readdir(), closedir() )
+
+
+*/
+
+
+#include<stdio.h>
+#include<unistd.h>
+#include<dirent.h>
+#include<stdlib.h>
+
+int main(){
+    DIR *dir;
+    struct dirent * entry;
+
+    dir=opendir(".");
+    if(dir==NULL){
+           printf("unable to open directory .....\n");
+           return 1;
+    }
+    printf("contents of the current directory ....\n");
+    while((entry=readdir(dir))!=NULL)
+    {
+        printf("%s\n",entry->d_name);
+
+    }
+    closedir(dir);
+
+    return 0;
+}
