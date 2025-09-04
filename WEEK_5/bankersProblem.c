79635d348ebc16f5d176dc55abd535a0a7050029
Escape <Escape>
2025-09-04 10:20:53 +0530

    week4 and week 5

---
+++ WEEK_5/bankersProblem.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+struct Process
+{
+    int A, B, C;
+};
+
+int main()
+{
+    int n, A, B, C;
+    printf("Enter the Number of Process - ");
+    scanf("%d", &n);
+
+    struct Process allocated[n];
+    struct Process require[n];
+    struct Process Need[n];
+    int isExecute[n];
+    int ans[n];
+
+    for (int i = 0; i < n; i++)
+        isExecute[i] = 0;
+    int al_a = 0;
+    int al_b = 0;
+    int al_c = 0;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the Allocated and Required Resources of P%d - ", i);
+        scanf("%d%d%d", &allocated[i].A, &allocated[i].B, &allocated[i].C);
+        scanf("%d%d%d", &require[i].A, &require[i].B, &require[i].C);
+
+        al_a += allocated[i].A;
+        al_b += allocated[i].B;
+        al_c += allocated[i].C;
+
+        Need[i].A = require[i].A - allocated[i].A;
+        Need[i].B = require[i].B - allocated[i].B;
+        Need[i].C = require[i].C - allocated[i].C;
+    }
+
+    printf("Enter the Aavilable Resources = ");
+    scanf("%d%d%d", &A, &B, &C);
+    A -= al_a;
+
+    B -= al_b;
+    C -= al_c;
+
+    int idx = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (isExecute[i] == 0)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (isExecute[j] == 0)
+                {
+                    if (Need[j].A <= A && Need[j].B <= B && Need[j].C <= C)
+                    {
+                        A += allocated[j].A;
+                        B += allocated[j].B;
+                        C += allocated[j].C;
+
+                        ans[idx] = j;
+                        idx++;
+                        isExecute[j] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (isExecute[i] == 0)
+        {
+            printf("No Safe Sequence is Possible");
+            return 0;
+        }
+    }
+
+    printf("The Safe Sequence is - ");
+    for (int i = 0; i < n - 1; i++)
+    {
+        printf("P%d ->", ans[i]);
+    }
+    printf("P%d", ans[n - 1]);
+    return 0;
+}
