8411ee7b30c217651dd62775f682236d1c65775d
PRIYANSHU_PANT <pantpriyanshu707@gmail.com>
2025-10-29 18:32:13 +0000

    updated pipe file

--- WEEK_6/QUESTION_1/1_pipe.c
+++ WEEK_6/QUESTION_1/1_pipe.c
@@ -1,19 +1,33 @@
-#include<stdio.h>
-#include<unistd.h>
-int main(){
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
     int p[2];
-    int returnstatus;
-    char writing[2][25]={"hello","word"};
-    char readmsg[25];
-   
-    returnstatus=pipe(p);
-    if(returnstatus==1){
-        printf("pipe not created");
+    pid_t pid;
+    char msg[] = "Hello from child";
+    char buffer[50];
+
+    if (pipe(p) == -1) {
+        printf("Pipe creation failed!\n");
+        return 1;
     }
-    printf("\n writing started %s", writing[0]);
-    write(p[1],writing[0],sizeof(writing[0]));
-    read(p[0],readmsg,sizeof(readmsg));
-    printf("\n Reading from pipe-msg 1 %s\n",readmsg);
-    
+
+    pid = fork();  // create child process
+
+    if (pid == 0) {
+        // Child Process
+        close(p[0]); // Close unused read end
+        write(p[1], msg, strlen(msg) + 1);
+        printf("Child: message sent to parent.\n");
+        close(p[1]);
+    } else {
+        // Parent Process
+        close(p[1]); // Close unused write end
+        read(p[0], buffer, sizeof(buffer));
+        printf("Parent: received message: %s\n", buffer);
+        close(p[0]);
+    }
+
     return 0;
 }
