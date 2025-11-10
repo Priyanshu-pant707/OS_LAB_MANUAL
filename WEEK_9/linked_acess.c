ee7cddbe05cdbb432cc1a4cc503c8799ef7bddee
PRIYANSHU_PANT <pantpriyanshu707@gmail.com>
2025-11-10 03:50:31 +0000

    week11

---
+++ WEEK_9/linked_acess.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+struct linked_Access{
+    int data;   // 
+    int next_val;
+};
+
+
+int main(){
+    int n;
+    printf("enter the number of process");
+ 
+     for(int i=0;i<n;i++){
+        printf("enter the first block index of the prcess %d : ",i+1);
+        while(1){
+
+            printf("enter -1 to end the list /n");
+            printf("otherwise enter the next memory block");
+
+            
+            
+        }
+     }
+    
+}
